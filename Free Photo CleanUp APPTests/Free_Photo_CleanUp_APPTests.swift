ece5a52a06b646825867dc9c7922732a3f2e2724
Chia-Wei <chang.ch23@gmail.com>
2025-07-28 20:21:45 -0400

    Initial Commit

---
+++ Free Photo CleanUp APPTests/Free_Photo_CleanUp_APPTests.swift
@@ -0,0 +1,16 @@
+//
+//  Free_Photo_CleanUp_APPTests.swift
+//  Free Photo CleanUp APPTests
+//
+//  Created by chang chiawei on 2025-07-28.
+//
+
+import Testing
+
+struct Free_Photo_CleanUp_APPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Free Photo CleanUp APPUITests/Free_Photo_CleanUp_APPUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Free_Photo_CleanUp_APPUITestsLaunchTests.swift
+//  Free Photo CleanUp APPUITests
+//
+//  Created by chang chiawei on 2025-07-28.
+//
+
+import XCTest
+
+final class Free_Photo_CleanUp_APPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
